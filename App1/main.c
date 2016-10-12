ad2f4e2b2dcb9e3cf7be8317804192f76334e69d
Julie LALLEE <julie.lallee@gmail.com>
2016-10-12 23:40:17 +0200

    Premier commit : Installation du projet

---
+++ App1/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: Julie
+ *
+ * Created on 12 octobre 2016, 23:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("HELLO JULIE");
+    
+    return (EXIT_SUCCESS);
+}
+
